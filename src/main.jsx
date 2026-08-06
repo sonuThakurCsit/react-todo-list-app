d476d19a2ffb5fe31384f9f6532b8827dc46ba62
Sonu Kumar <sonuthakurcsit@gmail.com>
2026-08-06 23:10:35 +0530

    Customize toast notification styles

--- src/main.jsx
+++ src/main.jsx
@@ -6,10 +6,37 @@ import { Toaster } from "react-hot-toast"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-     <Toaster
+  <App />
+
+  <Toaster
     position="top-right"
     reverseOrder={false}
+    toastOptions={{
+      duration: 3000,
+
+      style: {
+        background: "#1e293b",
+        color: "#fff",
+        borderRadius: "10px",
+        padding: "16px",
+        fontSize: "15px",
+      },
+
+      success: {
+        iconTheme: {
+          primary: "#22c55e",
+          secondary: "#fff",
+        },
+      },
+
+      error: {
+        iconTheme: {
+          primary: "#ef4444",
+          secondary: "#fff",
+        },
+      },
+    }}
   />
-  </StrictMode>,
+
+</StrictMode>,
 )
